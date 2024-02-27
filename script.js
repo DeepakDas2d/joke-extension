f43d63d8a6ff675b81791fc1dbeccc7042315793
deepakdas2d <contacttodeepakdas@gmail.com>
2024-02-27 18:32:21 +0530

    added hindi & eng jokes

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-let apiKey = "844654189a6b05c47cf0b6daac44";
 let hindiApi =
   "https://hindi-jokes-api.onrender.com/jokes?api_key=844654189a6b05c47cf0b6daac44";
-let url = "https://icanhazdadjoke.com/slack";
-fetch(hindiApi)
-  .then((data) => data.json())
-  .then((jokeData) => {
-    // const jokeText = jokeData.attachments[0].text;
-    const jokeText = jokeData.jokeContent;
-    const jokeElement = document.getElementById("jokeElement");
-    jokeElement.innerHTML = jokeText;
-    setBgColor();
-  });
+let engApi = "https://icanhazdadjoke.com/slack";
 
-let colors = ["red", "blue", "green", "crimson", "pink", "black"];
-let randumNumber = Math.ceil(Math.random() * 5);
-// console.log(colors[randumNumber]);
 const setBgColor = () => {
+  let colors = ["red", "blue", "green", "crimson", "pink", "black"];
+  let randumNumber = Math.ceil(Math.random() * 5);
   let jokeElement = document.getElementById("jokeElement");
   jokeElement.style.backgroundColor = colors[randumNumber];
 };
+
+function hindiJokes() {
+  const jokeElement = document.getElementById("jokeElement");
+  jokeElement.innerHTML = "Loading..."; // Display Loading message
+  fetch(hindiApi)
+    .then((data) => data.json())
+    .then((jokeData) => {
+      const jokeText = jokeData.jokeContent;
+      jokeElement.innerHTML = jokeText;
+      setBgColor();
+    });
+}
+
+function englishJokes() {
+  const jokeElement = document.getElementById("jokeElement");
+  jokeElement.innerHTML = "Loading...";
+  fetch(engApi)
+    .then((data) => data.json())
+    .then((jokeData) => {
+      const jokeText = jokeData.attachments[0].text;
+      jokeElement.innerHTML = jokeText;
+      setBgColor();
+    });
+}
