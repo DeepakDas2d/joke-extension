f255cd29bee5210fffc77918d8fe6a8a1fdbfba4
deepakdas2d <contacttodeepakdas@gmail.com>
2024-02-27 18:42:13 +0530

    bug fixed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let hindiApi =
   "https://hindi-jokes-api.onrender.com/jokes?api_key=844654189a6b05c47cf0b6daac44";
 let engApi = "https://icanhazdadjoke.com/slack";
+let colors = ["red", "blue", "green", "crimson", "pink", "black"];
 
 const setBgColor = () => {
-  let colors = ["red", "blue", "green", "crimson", "pink", "black"];
-  let randumNumber = Math.ceil(Math.random() * 5);
   let jokeElement = document.getElementById("jokeElement");
-  jokeElement.style.backgroundColor = colors[randumNumber];
+  const randomNumber = Math.floor(Math.random() * colors.length);
+  jokeElement.style.backgroundColor = colors[randomNumber];
 };
 
 function hindiJokes() {
@@ -32,3 +32,10 @@ function englishJokes() {
       setBgColor();
     });
 }
+
+document
+  .getElementById("hindiJokesButton")
+  .addEventListener("click", hindiJokes);
+document
+  .getElementById("englishJokesButton")
+  .addEventListener("click", englishJokes);
